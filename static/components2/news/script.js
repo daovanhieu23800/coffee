3406e321efb80dffecfa8a6e77ae06a060e9d80f
daovanhieu23800 <73016968+daovanhieu23800@users.noreply.github.com>
2021-12-10 22:49:48 +0700

    test

--- static/components2/news/script.js
+++ static/components2/news/script.js
@@ -1,6 +1,6 @@
 
 const  handleNews = async ()=>{
-    const response = await fetch('http://https://coffee-ewebsite.herokuapp.com/getnews/');
+    const response = await fetch('http://https://127.0.0.1:8000/getnews/');
     const myJson = await response.json();
     let newJson = [];
     if(myJson.length>6)
